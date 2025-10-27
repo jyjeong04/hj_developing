4f9e6d64b59fd3bbe075f569a08f659c6f3b7cb6
jy <jy.jeong.04@gmail.com>
2025-10-27 18:57:42 +0900

    10.27 end

--- hj/hj.hpp
+++ hj/hj.hpp
@@ -1,46 +1,47 @@
 #pragma once
 
-#include "param.hpp"
-#include "cl.hpp"
 #include <CL/cl_platform.h>
-#include <array>
 #include <cstdint>
 #include <iostream>
 #include <sys/types.h>
 #include <vector>
-#include <memory>
 
 struct Tuple {
-    uint32_t key;
-    uint32_t rid;
+  uint32_t key;
+  uint32_t rid;
 
-    friend std::ostream &operator<<(std::ostream &out, const Tuple &tuple);
+  friend std::ostream &operator<<(std::ostream &out, const Tuple &tuple);
 };
 
 struct JoinedTuple {
-    uint32_t key;
-    uint32_t ridR;
-    uint32_t ridS;
+  uint32_t key;
+  uint32_t ridR;
+  uint32_t ridS;
 
-    friend std::ostream &operator<<(std::ostream &out, const JoinedTuple &tuple);
+  friend std::ostream &operator<<(std::ostream &out, const JoinedTuple &tuple);
 };
 
 struct KeyHeader {
-    uint32_t key{0};
-    std::vector<uint32_t> ridList;
+  uint32_t key{0};
+  std::vector<uint32_t> ridList;
 };
 
 struct BucketHeader {
-    uint32_t totalNum{0};
-    std::vector<KeyHeader> keyList;
+  uint32_t totalNum{0};
+  std::vector<KeyHeader> keyList;
 };
 
 inline std::ostream &operator<<(std::ostream &out, const Tuple &tuple) {
-    out << "Tuple{" << tuple.key << "," << tuple.rid << "}";
-    return out;
+  out << "Tuple{" << tuple.key << "," << tuple.rid << "}";
+  return out;
 }
 
 inline std::ostream &operator<<(std::ostream &out, const JoinedTuple &tuple) {
-    out << "Joined{" << tuple.key << ", R:" << tuple.ridR << ", S:" << tuple.ridS << "}";
-    return out;
+  out << "Joined{" << tuple.key << ", R:" << tuple.ridR << ", S:" << tuple.ridS
+      << "}";
+  return out;
 }
+
+// Simple Hash Join using OpenCL
+std::vector<JoinedTuple> shj(const std::vector<Tuple> &R,
+                             const std::vector<Tuple> &S, cl_uint deviceIndex);
